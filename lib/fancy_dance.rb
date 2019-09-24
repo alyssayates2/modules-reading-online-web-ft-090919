fc9c098486e5b74ca3401b053ffb2e8430549d8b
Alyssa Yates <bosslys@icloud.com>
2019-09-24 23:17:15 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,5 +1,6 @@
-module FancyDance :: MetaDancing
+module FancyDance
   module InstanceMethods
+
     def twirl
       "I'm twirling!"
     end
@@ -14,6 +15,13 @@ module FancyDance :: MetaDancing
 
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+  end
+
+  module ClassMethods
+
+    def metadata
+      "This class produces objects that love to dance."
     end
   end
 end
